e1758cb93d6df5d8f6cdc13228a50082ccc6ba4f
Ruan-Hasselmann <52681260+Ruan-Hasselmann@users.noreply.github.com>
2023-11-16 21:55:47 -0300

    Pequenos ajustes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><h2>Home</h2></div>
+    <div>
+      <h2>Home</h2>
+      <h1>Seja bem vindo ao sistema de crediário</h1>
+    </div>
   )
 }
 
