d2496c862584ce08c05d8b650e67e6b03e2edda4
Ruan-Hasselmann <52681260+Ruan-Hasselmann@users.noreply.github.com>
2023-11-17 01:06:58 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
       <h2>Seja bem vindo ao sistema de crediário</h2>
+      <h2>Em contante evolução!!</h2>
     </div>
   )
 }
